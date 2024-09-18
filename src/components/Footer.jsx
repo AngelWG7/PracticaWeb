4dedf052f528693e761122e7dafca85989e1e574
Julio <julio.ce01@hotmail.com>
2024-09-18 13:43:35 -0600

    PRC-2 creacion del footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Contacto: contacto@bellezatotal.com | Tel: (123) 456-7890</p>
+      <ul>
+        <li><a href="https://facebook.com/" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+        <li><a href="https://instagram.com/" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+        <li><a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
